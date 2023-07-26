bf322775d621a98013137ffda8c6b9ab44b6fae8
RaNoronha <ranlcgnn@gmail.com>
2023-07-26 12:55:59 -0300

    Feat: criação de método auxiliar

--- ProdutosApp/ProdutosApp.Services/Controllers/ProdutosController.cs
+++ ProdutosApp/ProdutosApp.Services/Controllers/ProdutosController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProdutosApp.Data.Entities;
+using ProdutosApp.Data.Repositories;
 using ProdutosApp.Services.Models;
 
 namespace ProdutosApp.Services.Controllers
@@ -8,12 +10,38 @@ namespace ProdutosApp.Services.Controllers
     [ApiController]
     public class ProdutosController : ControllerBase
     {
+        #region POST
+
         [HttpPost]
         [ProducesResponseType(typeof(ProdutosGetModel), 200)]
         public IActionResult Post([FromBody] ProdutosPostModel model)
         {
-            return Ok();
+            try
+            {
+                var produto = new Produto();
+                var produtoRepository = new ProdutoRepository();
+                
+
+                produto.Id = Guid.NewGuid();
+                produto.Nome = model.Nome;
+                produto.Preco = model.Preco;
+                produto.Quantidade = model.Quantidade;
+                produto.DataHoraCriacao = DateTime.Now;
+                produto.DataHoraUltimaAtualizacao = DateTime.Now;
+
+                produtoRepository.Cadastrar(produto);                
+
+                return StatusCode(200, EntityToModel(produto));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { e.Message });
+            }
         }
+
+        #endregion
+
+        #region PUT
 
         [HttpPut]
         [ProducesResponseType(typeof(ProdutosGetModel), 200)]
@@ -22,12 +50,20 @@ namespace ProdutosApp.Services.Controllers
             return Ok();
         }
 
+        #endregion
+
+        #region DELETE
+
         [HttpDelete("{id}")]
         [ProducesResponseType(typeof(ProdutosGetModel), 200)]
         public IActionResult Delete(Guid? id)
         {
             return Ok();
         }
+
+        #endregion
+
+        #region PESQUISAS
 
         [HttpGet]
         [ProducesResponseType(typeof(List<ProdutosGetModel>), 200)]
@@ -42,5 +78,26 @@ namespace ProdutosApp.Services.Controllers
         {
             return Ok();
         }
+
+        #endregion
+
+        #region Métodos Auxiliares
+
+        private ProdutosGetModel EntityToModel(Produto produto)
+        {
+            var resultado = new ProdutosGetModel();
+
+            resultado.Id = produto.Id;
+            resultado.Nome = produto.Nome;
+            resultado.Preco = produto.Preco;
+            resultado.Quantidade = produto.Quantidade;
+            resultado.Total = produto.Preco * produto.Quantidade;
+            resultado.DataHoraCriacao = produto.DataHoraCriacao;
+            resultado.DataHoraUltimaAtualizacao = produto.DataHoraUltimaAtualizacao;
+
+            return resultado;
+        }
+
+        #endregion
     }
 }
--- ProdutosApp/ProdutosApp.Services/Models/ProdutosGetModel.cs
+++ ProdutosApp/ProdutosApp.Services/Models/ProdutosGetModel.cs
@@ -8,6 +8,7 @@ namespace ProdutosApp.Services.Models
         public string? Nome { get; set; }
         public decimal? Preco { get; set; }
         public int? Quantidade { get; set; }
+        public decimal? Total { get; set; }
         public DateTime? DataHoraCriacao { get; set; }
         public DateTime? DataHoraUltimaAtualizacao { get; set; }
     }
